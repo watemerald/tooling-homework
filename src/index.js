e24e5f56d550badf6f2fd30e6b1ecf7ba9c3cebb
Andrii Sherman <andriisherman@gmail.com>
2020-06-15 12:14:44 -0400

    Asset setup (css, png, csv)

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import _ from "lodash";
+import "./style.css";
+import Icon1 from "./icons/icon.png";
+import Icon2 from "./icons/icon.svg";
 
 function component() {
   const element = document.createElement("div");
 
   element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.classList.add("hello");
+
+  const myIcon1 = new Image();
+  myIcon1.src = Icon1;
+
+  element.appendChild(myIcon1);
+
+  const myIcon2 = new Image();
+  myIcon2.src = Icon2;
+
+  element.appendChild(myIcon2);
 
   return element;
 }
